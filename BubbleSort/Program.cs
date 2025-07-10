4b3e34852cd2858645297522cd2c1ad268949ac2
Sachin <sachin.kharabanda@aol.com>
2025-07-10 11:58:28 +0100

    Completed algorithm

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] BubbleSort(int[] x) {
+
+    for (int i = 0; i < x.Length; i++) { 
+    
+        for (int j = 0; j < x.Length-1; j++) {
+
+            if (x[j+1] < x[j]) {
+                int temp = x[j];
+                x[j] = x[j+1];
+                x[j+1] = temp;
+            }
+        }
+    
+    }
+
+    return x;
+}
+
+int[] a = { 7, 12, 9, 11, 3 };
+a = BubbleSort(a);
+foreach (int b in a) {Console.Write(b + ",");}
